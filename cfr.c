d95b875aef4ca94d653af66fa6736462aca2d837
Masato <mabonakai@gmail.com>
2018-01-03 13:42:42 +0900

    renew cfr.c commit

--- cfr.c
+++ cfr.c
@@ -8,8 +8,8 @@
 #include <math.h>
 
 #define NUM_ACTION 2
-#define NUM_MEMBER 2
-#define NUM_CARDS  3
+#define NUM_MEMBER 3
+#define NUM_CARD   5  //52
 
 #define PASS 0
 #define BET  1
@@ -37,8 +37,8 @@ char *g_view[2]={"Pose ","Bet  "};
 NODE *getNode(char *);
 NODE *createNode(int *);
 int  setNode(NODE*,char *);
-int  train(int);
-double cfr(int *,char *,double,double,int *);
+int  train(int,FILE *);
+double cfr(int *,char *,char *,double*,int *);
 
 
 /**
@@ -91,39 +91,47 @@ NODE *pNode;
 /***
  áB
 ****/
-int toString(pNode)
+int toString(pNode,fw)
 NODE *pNode;
+FILE *fw;
 {
    /* func call */
    int a;
 
    getAverageStrategy(pNode);
-   fprintf(stderr,"%4s\n",pNode->infoSet);
+   fprintf(fw,"%4s\n",pNode->infoSet);
    for(a = 0;a < pNode->num;a++) {
-     fprintf(stderr,"   action=%s avrage=%lf \n",g_view[a],pNode->avgStrategy[a]);
+     fprintf(fw,"   action=%s avrage=%lf \n",g_view[a],pNode->avgStrategy[a]);
    }
    return(1);
 }
 /***
   áD
 ***/
-int train(iteration)
+int train(iteration,fw)
 int iteration;
+FILE *fw;
 {
   int i;
   int numA;
   
-  int cards[3]={1,2,3};
+  int cards[NUM_CARD];
   int c1,c2,itemp,numCards;
   double util;
   char history[256];
-
+  char infoSet[256];
+  
   int ret;
 
-  numCards = NUM_CARDS;
+  int j;
+  double pd[NUM_MEMBER];
+
+  numCards = NUM_CARD;
   numA = NUM_ACTION;
 
   util=0;
+  for(i = 0; i< numCards;i++) cards[i]=i+1;
+
   for(i =0;i < iteration;i++) {
     /* Shuffle cards áE*/
     for(c1 = numCards-1; c1 > 0; c1--) {
@@ -134,13 +142,15 @@ int iteration;
     }
     /* call CFR Å@áF*/
     history[0]='\0';
-    util += cfr(cards,history,1.0,1.0,&ret);
+    infoSet[0]='\0';
+    for(j=0;j<NUM_MEMBER;j++) pd[j] = 1.0;
+    util += cfr(cards,infoSet,history,pd,&ret);
     if(ret == FATAL) return(ret);
   }
-  fprintf(stderr,"Average game value:%lf \n",util / iteration); 
+  fprintf(fw,"Average game value:%lf \n",util / iteration); 
   /* print out node */
   for(i=0;i<g_nodeNum;i++) {
-    toString(g_pNodes[i]);
+    toString(g_pNodes[i],fw);
   }
 
   return(0);
@@ -148,16 +158,17 @@ int iteration;
 /***
   CFRÅ@áF
 ***/
-double cfr(cards,history,p0,p1,pRet)
+double cfr(cards,infoSet,history,pd,pRet)
 int *cards;
+char *infoSet;
 char *history;
-double p0;
-double p1;
+double *pd; /* NUM_MEMBER */
 int *pRet;
 {
 
   int round;
   int player;
+  int player1;
   int opponent;
 
   char s_card[3];
@@ -166,53 +177,103 @@ int *pRet;
   double nodeUtil=0;
   char nextHistory[256];
   int a,ret;
-  char infoSet[256];
+  char nextInfoSet[256];
   double regret;
+
+  int j;
+  double pdd[NUM_MEMBER];
+  char s_player[3];
+  double pdave;
+
+  double rnd;
+  double limit[NUM_MEMBER+1];
   
   round = strlen(history);
+#if 0
   player = round % NUM_MEMBER;
+#else
+  player = 0;
+#endif
   opponent = (player == 0 ? 1 : 0);
+
+#if 0
+  player1 = round % NUM_MEMBER;
+#else
+  limit[0]=0.0;
+  for(j = 1;j < NUM_MEMBER;j++) {
+    limit[j] = limit[j-1] + 1.0/(double)NUM_MEMBER; 
+  }
+  limit[NUM_MEMBER]=1.0;
+
+  rnd = (double)rand()/(double)RAND_MAX;
+  for(j = 0;j < NUM_MEMBER;j++) {
+    if(rnd >= limit[j] && rnd < limit[j+1]) break;
+  }
+  player1 = j;
+#endif
 
   /* Return payoff for terminal state áG */
   if(round > 1) {
     if(history[round-1] == 'p') {
       if(!strncmp(history,"pp",2)) {
-        if(cards[player] > cards[opponent]) return(1);   /* pp */
+        if(cards[player] > cards[opponent]) return( 1);  /* pp */
         else                                return(-1);  /* pp */
       }
-      else  return(1);                                   /* bp */
+      else  return(-1);                                   /* bp */
     }
     else
     if(!strncmp(&history[round-2],"bb",2)) {
-        if(cards[player] > cards[opponent]) return(2);   /* bb */
+        if(cards[player] > cards[opponent]) return( 2);  /* bb */
         else                                return(-2);  /* bb */
     }
   }
   /* round = 0 or 1 */
-  sprintf(s_card,"%2d",cards[player]);s_card[3]='\0';
-  strcpy(infoSet,s_card);
-  strcat(infoSet,history);
+#if 0
+  sprintf(s_card,"%d",cards[0]);s_card[2]='\0';
+#else
+  sprintf(s_card,"%d",cards[player]);s_card[2]='\0';
+#endif
+  strcpy(nextInfoSet,s_card);strcat(nextInfoSet,"card ");
+
+#if 1
+  sprintf(s_player,"%d",player1+1);s_player[2]='\0';
+  if(round > 0) strcat(nextInfoSet,s_player);
+#endif
+
+  strcat(nextInfoSet,history);
+
   /* Get informatin set node or create if if nonexistent áH */
-  pNode=getNode(infoSet);
+  pNode=getNode(nextInfoSet);
   if(pNode == NULL) {
     pNode = createNode(&ret);
     if(ret == FATAL) {
       *pRet = ret;
       return(0);
     }
-    setNode(pNode,infoSet);
+    setNode(pNode,nextInfoSet);
   }
   /* for each acton, recursivly call CFR with additional history and probability áI */
-  if(player == 0) getStrategy(pNode,p0);
-  else            getStrategy(pNode,p1);
+  if(!strcmp(nextInfoSet,"1")) {
+    if(g_dbg) fprintf(stderr,"3pb");
+  }
+
+  for(j = 0;j < NUM_MEMBER;j++) {
+    if(j == player) getStrategy(pNode,pd[j]);
+  }
+
   
   nodeUtil = 0;
   for(a = 0;a < pNode->num; a++) {
     strcpy(nextHistory,history);
     if(a == 0) strcat(nextHistory,"p");
     if(a == 1) strcat(nextHistory,"b");
-    if(player == 0) util[a] = cfr(cards,nextHistory,p0*pNode->strategy[a],p1,&ret);
-    else            util[a] = cfr(cards,nextHistory,p0,p1*pNode->strategy[a],&ret);
+
+    for(j=0;j<NUM_MEMBER;j++) {
+      if(j == player1) pdd[j] = pd[j] * pNode->strategy[a];
+      else             pdd[j] = pd[j];
+    }
+    util[a] = cfr(cards,nextInfoSet,nextHistory,pdd,&ret);
+
     if(ret == FATAL) {
       *pRet=ret;
       return(0.0);
@@ -222,8 +283,21 @@ int *pRet;
   /* for each action, compute and accumulate counterfactual regret áJ */
   for(a = 0;a < pNode->num; a++) {
     regret = util[a] - nodeUtil;
-    if(player == 0) pNode->regretSum[a] = p1 * regret;
-    else            pNode->regretSum[a] = p0 * regret;
+
+#if 0
+    if(player1 == 0) pNode->regretSum[a] += pd[1] * regret;
+    else             pNode->regretSum[a] += pd[0] * regret;
+#else
+    pdave=0;
+    for(j = 0;j< NUM_MEMBER;j++) {
+      if(player1 == j) continue;
+      pdave += pd[j];
+    }
+    pdave /= (NUM_MEMBER-1);  //average except player1
+ 
+    pNode->regretSum[a] += pdave * regret;
+
+#endif
   }
 
   *pRet=0;
@@ -236,9 +310,28 @@ int main(argc,argv)
 int argc;
 char *argv[];
 {
+   int iteration;
+   FILE *fw;
 
+   if(argc == 1) {
+     fprintf(stderr,"USAGE numOfIteration outFileName\n");
+     exit(9);
+   }
+   else 
+   if(argc == 2) {
+     iteration = atoi(argv[1]);
+     fw=stderr;
+   }
+   else 
+   if(argc == 3) {
+     iteration = atoi(argv[1]);
+     if(!(fw=fopen(argv[2],"w"))) exit(2);
+   }
+          
    /* trainning */
-   train(1000);
+   train(iteration,fw);
+
+   if(fw != stderr) fclose(fw);
 
    /* result */
    return(0);
